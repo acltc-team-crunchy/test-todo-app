ed3b77efbe529efc687c516b0dbd36c305fe9808
Mark Richardson <markeban@yahoo.com>
2016-05-05 21:02:57 -0700

    finished tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -14,4 +14,39 @@ RSpec.describe Task, type: :model do
       expect(task.complete).to eq(true)
     end
   end
+
+  describe '#toggle_favorite' do
+    it 'should switch favorite to true if it began as false' do
+      task = Task.new(favorite: false)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(true)
+    end
+
+    it 'should switch favorite to true if it began as false' do
+      task = Task.new(favorite: false)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(true)
+    end
+  end
+
+  describe '#overdue?' do
+    it 'should return true if deadline of task is earlier than now' do
+      task = Task.new(deadline: 1.day.ago)
+      expect(task.overdue?).to eq(true)    
+    end
+
+    it 'should return false if deadline of task is later than now' do
+      task = Task.new(deadline: 1.day.from_now)
+      expect(task.overdue?).to eq(false)
+    end
+  end
+
+  describe '#snooze_hour!' do
+    it 'should push deadline off by one hour' do
+      deadline = DateTime.new(2016, 10, 31)
+      task = Task.new(deadline: deadline)
+      task.snooze_hour!
+      expect(task.deadline).to eq(deadline + 1.hour)
+    end
+  end
 end
